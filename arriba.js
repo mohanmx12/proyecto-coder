eb5accc242250296f60031d50fece9e06e73bf66
Milan Mohanna <milan.mohanna@gmail.com>
2022-01-17 18:03:06 -0300

    segunda actualizacion de take a walk para el final

--- arriba.js
+++ arriba.js
@@ -14,4 +14,35 @@ $(window).scroll(function(){
     }
 });
 
+$(".logo").click(function(){
+       $("body, html").animate({
+           scrollTop: "0px"
+       }, 300);
+
+    });
+
+$(window).scroll(function(){
+    if( $(this).scrollTop() > 0 ){
+        $(".logo").slideUp(300);
+    } else {
+        $(".logo").slideDown(300);
+    }
+});
+
+$(".titulo_principal").click(function(){
+    $("body, html").animate({
+        scrollTop: "0px"
+    }, 300);
+
+ });
+
+$(window).scroll(function(){
+ if( $(this).scrollTop() > 0 ){
+     $(".titulo_principal").slideDown(300);
+ } else {
+     $(".titulo_principal").slideUp(300);
+ }
+});
+
+
 });
